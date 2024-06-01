221946991dcc69d1df63e39637ef0907055dd67c
nam-web <65065658+nam-web@users.noreply.github.com>
2024-06-01 16:32:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	var d1 = new Date(date1);
+	var d2 = new Date(date2);
+	var utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
+	var utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
+	var diffInMillis = utc2 - utc1;
+	var diffInDays = diffInMillis/(1000*60*60*24);
+	return diffInDays;
 };
 
 // Do not change the code below.
